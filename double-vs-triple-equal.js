f6ccea68741e440eeb25d63770f126481457c91b
ibrahim Munna <67519588+hossainmunna@users.noreply.github.com>
2020-08-08 07:50:55 +0600

    double equal vs triple equal for  interveiw

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,11 @@
+const first = 1 ;
+const second = true ;
+
+// double equal dile konta kon type er oita dekhe na just millei condition true dekhay . ar triple equal dile aro kothin /strick vabe check korbe. 
+
+if(first == second){
+    console.log ("condition is true");
+}
+else{
+    console.log("condition is false")
+}
