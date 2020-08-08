08019aa4381d8bc62c2159775c8df11032904dde
ibrahim Munna <67519588+hossainmunna@users.noreply.github.com>
2020-08-08 07:07:58 +0600

    null vs undefinded cleared for interview

---
+++ null-vs-undefind.js
@@ -0,0 +1,31 @@
+                    //Undefined is negative or falsy ///
+var jenifa ;
+console.log (jenifa);
+
+function add (num1,num2){
+    console.log(num1 + num2);
+    // Missing the return here looks so undefined
+}
+const result = add (10,40);
+console.log(result);
+
+
+function add (num1, num2){
+    console.log(num1,num2);
+}
+const result = add(15);
+console.log(result);
+
+
+
+const Bua  = {name: "cat", phone: 34555};
+console.log(Bua.bf);
+
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,12];
+console.log(ages[11]);
+
+            //null.............
