3a561065e6965eb8fd44a03763ec1cae567fa272
ibrahim Munna <67519588+hossainmunna@users.noreply.github.com>
2020-08-08 07:30:08 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,23 @@
+               //Falsy....
+//0
+//""
+//undefined
+//null
+//NaN (Not a Number)
+//false
+
+
+                 //truthy....
+
+//'0', " "
+// []
+//{object}
+
+let name = 10;
+
+if (name  || name ==0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
