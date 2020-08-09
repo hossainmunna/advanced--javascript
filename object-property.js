8628c6f65fae2f0f9bb2605fe8d943daf611d149
ibrahim Munna <67519588+hossainmunna@users.noreply.github.com>
2020-08-09 07:26:50 +0600

    advance topice try more and more

---
+++ object-property.js
@@ -0,0 +1,32 @@
+const students = [
+    {id: 21, name: 'Omar'},
+    {id: 21, name: 'Mannaa'},
+    {id: 21, name: 'Mayur'},
+    {id: 21, name: 'Deep'}    
+];
+ const newNames = [];
+for (let i = 0; i < students.length; i++) {
+    const element = students[i].name;
+    newNames.push(element); 
+}
+console.log(newNames);
+
+
+const students = [
+    {id: 21, name: 'Omar'},
+    {id: 31, name: 'Mannaa'},
+    {id: 41, name: 'Mayur'},
+    {id: 71, name: 'Deepjol'}    
+];
+
+const names = students.map (s => s.name);
+ //console.log(names);
+
+const ids = students.map (s => s.id);
+//console.log(ids);
+
+const bigger = students.filter (s => s.id >40);
+//console.log(bigger);
+
+const finder = students.find (s => s.id >40);
+console.log(finder);
